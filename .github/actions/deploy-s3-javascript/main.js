816e4da7b6720b1bfa2d652028a263f263986328
Birgit Wright <wrightbirgit@gmail.com>
2025-07-16 21:29:51 +0200

    classic typo fix

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -6,7 +6,7 @@ const exec = require('@actions/exec');
 function run() {
     // Get some input values
     const bucket = core.getInput('bucket', {required: true});
-    const bucketRegion = core.getInput('bucket-regiom', {required: true});
+    const bucketRegion = core.getInput('bucket-region', {required: true});
     const distFolder = core.getInput('dist-folder', {required: true});
 
     // Upload files
