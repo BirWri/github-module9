15b44ea92c063d772aa4e34a97dca583ca1df341
Birgit Wright <wrightbirgit@gmail.com>
2025-07-16 21:26:37 +0200

    added custom deploy action to aws s3 bucket

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,8 +1,21 @@
+/* jshint esversion: 6 */
+
 const core = require('@actions/core');
-const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+    // Get some input values
+    const bucket = core.getInput('bucket', {required: true});
+    const bucketRegion = core.getInput('bucket-regiom', {required: true});
+    const distFolder = core.getInput('dist-folder', {required: true});
+
+    // Upload files
+    // could be done also by using AWS SDK
+    // here we use aws CLI
+
+    const s3URL = `s3://${bucket}`;
+    exec.exec(`aws s3 sync ${distFolder} ${s3URL} --region${bucketRegion}`);
+
     core.notice('Hello from my custom JavaSbript action!');
 }
 
